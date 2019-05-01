87252e053e1cfc9339c25b66bc2fa5465870621a
Brian Leathem <bleathem@netflix.com>
2019-04-30 21:26:11 -0700

    home-node-server serves the /trucks endpoint

--- packages/home-node-server/src/index.ts
+++ packages/home-node-server/src/index.ts
@@ -1,7 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 const port = 8080;
+import { fetchVehicleMatches } from 'ramtrucks-api';
 
-app.get('/', (_req: Request, res: Response) => res.send('Hello World!'));
+app.get('/trucks', async function(
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const vehicles = await fetchVehicleMatches();
+    res.json(vehicles);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get('/', (_req: Request, res: Response) => res.send('Hello World2!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
